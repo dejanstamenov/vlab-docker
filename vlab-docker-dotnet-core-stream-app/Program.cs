e67a4f30a05e888700f77b5d46ff9f497ab07fbf
Dejan Stamenov <stamenov.dejan@outlook.com>
2019-01-05 14:13:04 +0100

    Added console application that handles TCP sync communication on a given port.

---
+++ vlab-docker-dotnet-core-stream-app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace vlab_docker_dotnet_core_stream_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+#if DEBUG
+            args = new[] { "80" };
+#endif
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: dotnet run -p .\vlab-docker-dotnet-core-stream-app.csproj <port-number>");
+                return;
+            }
+
+            TCPServerSync tcpServerSync = new TCPServerSync(Convert.ToInt32(args[0]));
+        }
+    }
+}
