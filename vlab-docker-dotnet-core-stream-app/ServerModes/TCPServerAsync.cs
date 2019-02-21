6258d06ba064e2a3335aaac197131d313febd80c
Dejan Stamenov <stamenov.dejan@outlook.com>
2019-02-21 20:13:23 +0100

    Refactoring of the async server class. All methods are now being executed as async, including writing logs to output (console).

--- vlab-docker-dotnet-core-stream-app/ServerModes/TCPServerAsync.cs
+++ vlab-docker-dotnet-core-stream-app/ServerModes/TCPServerAsync.cs
@@ -24,62 +24,73 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
         public async Task RunServerAsync(CancellationToken cancellationToken)
         {
             cancellationToken.Register(serverListener.Stop);
-            Console.WriteLine($"Starting TCP server on port: {serverPortNumber}..");
+            Console.Out.WriteLineAsync($"Starting TCP server on port: {serverPortNumber}..");
+            // Console.WriteLine($"Starting TCP server on port: {serverPortNumber}..");
             serverListener.Start();
-            Console.WriteLine($"TCP server started. Awaiting client connection.");
+            Console.Out.WriteLineAsync($"TCP server started. Awaiting client connection.");
+            // Console.WriteLine($"TCP server started. Awaiting client connection.");
 
             while (!cancellationToken.IsCancellationRequested)
             {
                 try
                 {
                     TcpClient client = await serverListener.AcceptTcpClientAsync();
-                    HandleClientRequestAsync(client);
+                    await HandleClientRequestAsync(client);
                 }
                 catch (ObjectDisposedException) when (cancellationToken.IsCancellationRequested)
                 {
-                    Console.WriteLine("TcpListener stopped listening because cancellation was requested.");
+                    Console.Out.WriteLineAsync("TcpListener stopped listening because cancellation was requested.");
+                    // Console.WriteLine("TcpListener stopped listening because cancellation was requested.");
                 }
                 catch (Exception ex)
                 {
-                    Console.Error.WriteLine($"Exception awaiting for new TCP connection to the server.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
+                    Console.Out.WriteLineAsync($"Exception awaiting for new TCP connection to the server.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
+                    // Console.Error.WriteLine($"Exception awaiting for new TCP connection to the server.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
                 }
             }
         }
 
-        private void HandleClientRequestAsync(TcpClient client)
+        private async Task/*void*/ HandleClientRequestAsync(TcpClient client)
         {
             try
             {
                 using (StreamReader streamReader = new StreamReader(client.GetStream(), Encoding.UTF8))
                 {
-                    string clientMessage = string.Empty;
+                    string clientMessage = await streamReader.ReadLineAsync();
+                    await HandleRequest(client, clientMessage);
 
-                    while ((clientMessage = streamReader.ReadLine()) != null)
-                        HandleRequest(client, clientMessage);
+                    // string clientMessage = string.Empty;
+
+                    // while ((clientMessage = streamReader.ReadLine()) != null)
+                    //     HandleRequest(client, clientMessage);
                 }
             }
             catch (Exception ex)
             {
                 if (!client.Connected)
                 {
-                    Console.WriteLine($">> Client disconnected from server.");
+                    Console.Out.WriteLineAsync($">> Client disconnected from server.");
+                    // Console.WriteLine($">> Client disconnected from server.");
                     return;
                 }
 
-                Console.Error.WriteLine($"Exception reading stream from client.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
+                Console.Out.WriteLineAsync($"Exception reading stream from client.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
+                // Console.Error.WriteLine($"Exception reading stream from client.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
             }
         }
 
-        private void HandleRequest(TcpClient client, string clientMessage)
+        private async Task HandleRequest(TcpClient client, string clientMessage)
         {
-            Console.WriteLine($">> Client stream data from {client.Client.RemoteEndPoint as IPEndPoint}: {clientMessage}.");
+            Console.Out.WriteLineAsync($">> Client stream data from {client.Client.RemoteEndPoint as IPEndPoint}: {clientMessage}.");
+            // Console.WriteLine($">> Client stream data from {client.Client.RemoteEndPoint as IPEndPoint}: {clientMessage}.");
             // SendMessageToClient(client);
-            DisconnectClient(client);
+            await DisconnectClient(client);
         }
         
         private void SendMessageToClient(TcpClient client)
         {
-            Console.WriteLine($">> Sending thank you note to client: {client.Client.RemoteEndPoint as IPEndPoint}..");
+            Console.Out.WriteLineAsync($">> Sending thank you note to client: {client.Client.RemoteEndPoint as IPEndPoint}..");
+            // Console.WriteLine($">> Sending thank you note to client: {client.Client.RemoteEndPoint as IPEndPoint}..");
 
             if (client.Connected)
             {
@@ -88,20 +99,23 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
                 networkStreamWriter.WriteLine("Thank you!");
                 networkStreamWriter.Flush();
 
-                Console.WriteLine(">> Message to the client sent!");
+                Console.Out.WriteLineAsync(">> Message to the client sent!");
+                // Console.WriteLine(">> Message to the client sent!");
             }
             else
             {
-                Console.WriteLine(">> Cannot send message - client is disconnected.");
+                Console.Out.WriteLineAsync(">> Cannot send message - client is disconnected.");
+                // Console.WriteLine(">> Cannot send message - client is disconnected.");
             }
         }
 
-        private void DisconnectClient(TcpClient client)
+        private async Task DisconnectClient(TcpClient client)
         {
             if (client.Connected)
             {
+                Console.Out.WriteLineAsync($"Connection to client: {client.Client.RemoteEndPoint as IPEndPoint} is being closed..");
+                // Console.WriteLine($"Connection to client: {client.Client.RemoteEndPoint as IPEndPoint} is being closed..");
                 client.Close();
-                Console.WriteLine($"Connection to client: {client.Client.RemoteEndPoint as IPEndPoint} is closed.");
             }
         }
     }
