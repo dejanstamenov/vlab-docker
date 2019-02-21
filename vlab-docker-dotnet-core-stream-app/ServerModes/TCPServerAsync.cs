ba26450089ffb7b6c0df1d9816e44021b00c65ae
Dejan Stamenov <stamenov.dejan@outlook.com>
2019-02-21 21:47:39 +0100

    Clean up of the async server class. Now awaiting for the console output so that it is truly async.

--- vlab-docker-dotnet-core-stream-app/ServerModes/TCPServerAsync.cs
+++ vlab-docker-dotnet-core-stream-app/ServerModes/TCPServerAsync.cs
@@ -24,11 +24,9 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
         public async Task RunServerAsync(CancellationToken cancellationToken)
         {
             cancellationToken.Register(serverListener.Stop);
-            Console.Out.WriteLineAsync($"Starting TCP server on port: {serverPortNumber}..");
-            // Console.WriteLine($"Starting TCP server on port: {serverPortNumber}..");
+            await Console.Out.WriteLineAsync($"Starting TCP server on port: {serverPortNumber}..");
             serverListener.Start();
-            Console.Out.WriteLineAsync($"TCP server started. Awaiting client connection.");
-            // Console.WriteLine($"TCP server started. Awaiting client connection.");
+            await Console.Out.WriteLineAsync($"TCP server started. Awaiting client connection.");
 
             while (!cancellationToken.IsCancellationRequested)
             {
@@ -39,18 +37,16 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
                 }
                 catch (ObjectDisposedException) when (cancellationToken.IsCancellationRequested)
                 {
-                    Console.Out.WriteLineAsync("TcpListener stopped listening because cancellation was requested.");
-                    // Console.WriteLine("TcpListener stopped listening because cancellation was requested.");
+                    await Console.Out.WriteLineAsync("TcpListener stopped listening because cancellation was requested.");
                 }
                 catch (Exception ex)
                 {
-                    Console.Out.WriteLineAsync($"Exception awaiting for new TCP connection to the server.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
-                    // Console.Error.WriteLine($"Exception awaiting for new TCP connection to the server.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
+                    await Console.Out.WriteLineAsync($"Exception awaiting for new TCP connection to the server.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
                 }
             }
         }
 
-        private async Task/*void*/ HandleClientRequestAsync(TcpClient client)
+        private async Task HandleClientRequestAsync(TcpClient client)
         {
             try
             {
@@ -58,31 +54,23 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
                 {
                     string clientMessage = await streamReader.ReadLineAsync();
                     await HandleRequest(client, clientMessage);
-
-                    // string clientMessage = string.Empty;
-
-                    // while ((clientMessage = streamReader.ReadLine()) != null)
-                    //     HandleRequest(client, clientMessage);
                 }
             }
             catch (Exception ex)
             {
                 if (!client.Connected)
                 {
-                    Console.Out.WriteLineAsync($">> Client disconnected from server.");
-                    // Console.WriteLine($">> Client disconnected from server.");
+                    await Console.Out.WriteLineAsync($">> Client disconnected from server.");
                     return;
                 }
 
-                Console.Out.WriteLineAsync($"Exception reading stream from client.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
-                // Console.Error.WriteLine($"Exception reading stream from client.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
+                await Console.Out.WriteLineAsync($"Exception reading stream from client.\n{ex.Message}\n{ex.InnerException}\n{ex.StackTrace}");
             }
         }
 
         private async Task HandleRequest(TcpClient client, string clientMessage)
         {
-            Console.Out.WriteLineAsync($">> Client stream data from {client.Client.RemoteEndPoint as IPEndPoint}: {clientMessage}.");
-            // Console.WriteLine($">> Client stream data from {client.Client.RemoteEndPoint as IPEndPoint}: {clientMessage}.");
+            await Console.Out.WriteLineAsync($">> Client stream data from {client.Client.RemoteEndPoint as IPEndPoint}: {clientMessage}.");
             // SendMessageToClient(client);
             await DisconnectClient(client);
         }
@@ -90,7 +78,6 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
         private void SendMessageToClient(TcpClient client)
         {
             Console.Out.WriteLineAsync($">> Sending thank you note to client: {client.Client.RemoteEndPoint as IPEndPoint}..");
-            // Console.WriteLine($">> Sending thank you note to client: {client.Client.RemoteEndPoint as IPEndPoint}..");
 
             if (client.Connected)
             {
@@ -100,12 +87,10 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
                 networkStreamWriter.Flush();
 
                 Console.Out.WriteLineAsync(">> Message to the client sent!");
-                // Console.WriteLine(">> Message to the client sent!");
             }
             else
             {
                 Console.Out.WriteLineAsync(">> Cannot send message - client is disconnected.");
-                // Console.WriteLine(">> Cannot send message - client is disconnected.");
             }
         }
 
@@ -113,8 +98,7 @@ namespace vlab_docker_dotnet_core_stream_app.ServerModes
         {
             if (client.Connected)
             {
-                Console.Out.WriteLineAsync($"Connection to client: {client.Client.RemoteEndPoint as IPEndPoint} is being closed..");
-                // Console.WriteLine($"Connection to client: {client.Client.RemoteEndPoint as IPEndPoint} is being closed..");
+                await Console.Out.WriteLineAsync($"Connection to client: {client.Client.RemoteEndPoint as IPEndPoint} is being closed..");
                 client.Close();
             }
         }
